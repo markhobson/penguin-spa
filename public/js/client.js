cd03c74007bf055b1835372696527fd9b6fb4892
Mark Hobson <markhobson@gmail.com>
2013-02-09 11:35:35 +0000

    Used knockout-ext to move views into separate files

--- public/js/client.js
+++ public/js/client.js
@@ -8,6 +8,9 @@ require.config({
 		"jquery-json": "lib/jquery-json",
 		knockout: "//ajax.aspnetcdn.com/ajax/knockout/knockout-2.2.0",
 		"knockout-mapping": "//cdnjs.cloudflare.com/ajax/libs/knockout.mapping/2.3.4/knockout.mapping",
+		"knockout-ext": "lib/koExternalTemplateEngine-amd.min",
+		infuser: "lib/infuser-amd.min",
+		trafficCop: "lib/TrafficCop.min",
 		director: "lib/director.min",
 		bootstrap: "//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/js/bootstrap.min"
 	},
@@ -18,4 +21,4 @@ require.config({
 
 });
 
-require(["bootstrap", "route/routes", "error"]);
+require(["bootstrap", "knockout-ext", "route/routes", "error"]);

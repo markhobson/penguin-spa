51f0a0fbdca8af216f08f673f31bb15824a62ca3
Mark Hobson <markhobson@gmail.com>
2013-02-09 14:03:33 +0000

    Introduced createStory function

--- routes/story.js
+++ routes/story.js
@@ -8,10 +8,17 @@ exports.create = function(request, response)
 	var queueId = request.params.id;
 	var description = request.body.description;
 
-	var story = {"description": description};
+	var story = createStory(description);
 	queue.getQueue(queueId).stories.push(story);
 	queue.save();
 
 	// TODO: return story id when available
 	response.send(201);
+};
+
+var createStory = function(description)
+{
+	return {
+		description: description
+	};
 };

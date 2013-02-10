8805ab64a98895ae1f784196e5ddcadc8178f323
Mark Hobson <markhobson@gmail.com>
2013-02-10 12:34:14 +0000

    Formatting

--- route/queue.js
+++ route/queue.js
@@ -6,6 +6,8 @@ var queue = require("../resource/queue");
 module.exports = function(app)
 {
 	app.get("/api/queues", queue.list);
+	
 	app.get("/api/queue/:id", queue.get);
+	
 	app.post("/api/queues", queue.create);
 };

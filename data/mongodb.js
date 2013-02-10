15dd5f101c3054c4cea71429ede2566ac411d6dc
Mark Hobson <markhobson@gmail.com>
2013-02-10 19:45:11 +0000

    Formatting

--- data/mongodb.js
+++ data/mongodb.js
@@ -11,42 +11,37 @@ define(["mongodb"], function(mongodb) {
 		
 		findQueues: function(callback) {
 			client.connect(url, function(error, db) {
-				db.collection(queuesName)
-					.find()
-					.toArray(function(error, queues) {
-						callback(queues);
-						db.close();
-					});
+				db.collection(queuesName).find().toArray(function(error, queues) {
+					callback(queues);
+					db.close();
+				});
 			});
 		},
 
 		findQueue: function(id, callback) {
 			client.connect(url, function(error, db) {
-				db.collection(queuesName)
-					.findOne({_id: new mongodb.ObjectID(id)}, function(error, queue) {
-						callback(queue);
-						db.close();
-					});
+				db.collection(queuesName).findOne({_id: new mongodb.ObjectID(id)}, function(error, queue) {
+					callback(queue);
+					db.close();
+				});
 			});
 		},
 
 		saveQueue: function(queue, callback) {
 			client.connect(url, function(error, db) {
-				db.collection(queuesName)
-					.insert(queue, {w: 1}, function(error, queues) {
-						callback(queues[0]);
-						db.close();
-					});
+				db.collection(queuesName).insert(queue, {w: 1}, function(error, queues) {
+					callback(queues[0]);
+					db.close();
+				});
 			});
 		},
 
 		saveStory: function(queueId, story, callback) {
 			client.connect(url, function(error, db) {
-				db.collection(queuesName)
-					.update({_id: new mongodb.ObjectID(queueId)}, {$push: {stories: story}}, function(error, story) {
-						callback(story);
-						db.close();
-					});
+				db.collection(queuesName).update({_id: new mongodb.ObjectID(queueId)}, {$push: {stories: story}}, function(error, story) {
+					callback(story);
+					db.close();
+				});
 			});
 		}
 	};

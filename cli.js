81e0d356bf07e6f6b6ec1415dd03b3c98ad1c874
Mark Hobson <markhobson@gmail.com>
2013-02-20 22:33:55 +0000

    Default port to environment variable PORT if specified

--- cli.js
+++ cli.js
@@ -12,7 +12,7 @@ define(["optimist", "app"], function(optimist, app) {
 		.options("p", {
 			alias: "port",
 			describe: "Sets the server port",
-			"default": 8080
+			"default": process.env.PORT || 8080
 		});
 	
 	if (opts.argv.help) {

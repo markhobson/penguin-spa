5aeae49ac264dce1897dd0c257a189b69166edbe
Mark Hobson <markhobson@gmail.com>
2013-03-16 10:08:35 +0000

    Chained app functions together

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ if (config == null) {
 	cli.showHelp();
 }
 else {
-	var app = App.create(config.service.port, config.service.host);
 	console.log("Using web service " + config.service.host + ":" + config.service.port);
 	
-	app.listen(config.port, function() {
-		console.log("Server listening on port " + config.port);
-	});
+	App.create(config.service.port, config.service.host)
+		.listen(config.port, function() {
+			console.log("Server listening on port " + config.port);
+		});
 }

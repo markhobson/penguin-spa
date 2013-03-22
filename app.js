4780554dc8eba1417989ba4b846cb4d85ceba90e
Mark Hobson <markhobson@gmail.com>
2013-03-22 20:47:18 +0000

    Upgraded to Node 0.10, fixes #13

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ var path = require("path");
 exports.create = function(servicePort, serviceHost) {
 	
 	var app = express()
-		.use(express.favicon(path.join(this.__dirname, "public/img/penguin16.png")))
+		.use(express.favicon(path.join(__dirname, "public/img/penguin16.png")))
 		.use(express.logger("dev"))
 		.use("/api", httpProxy.createServer(servicePort, serviceHost))
 		.use(express.bodyParser())
 		.use(express.methodOverride())
-		.use(express.static(path.join(this.__dirname, "public")));
+		.use(express.static(path.join(__dirname, "public")));
 
 	// configure development profile
 
